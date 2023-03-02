c993aa78c282de729b43632351c01fde0373a09b
Bappa-Kamba <bappahmuhammad18@gmail.com>
2023-03-02 17:57:37 +0100

    omitted ';'

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -25,9 +25,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
     
     if (parent->left != NULL)
     {
-        new->left = parent->left
-        parent->left->parent = new
+        new->left = parent->left;
+        parent->left->parent = new;
     }
 
-    parent->left = new
+    parent->left = new;
 }
